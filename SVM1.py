3e8741488d642a97e7b0251df695366969297094
Ian Marshall <Marshalli1@wit.edu>
2018-07-30 07:03:02 -0700

    Have fully built out SVM....that doesn't work.

--- SVM1.py
+++ SVM1.py
@@ -1,24 +1,32 @@
 import numpy as np
+from numpy.random import RandomState
 import ta
 import matplotlib.pyplot as plt
 from matplotlib import style
 from sklearn import svm
 import functions
 import pandas as pd
+from sklearn.externals import joblib
 from sklearn import preprocessing
 
 style.use("ggplot")
+console_width = 320
+pd.set_option('display.width', console_width)
+np.set_printoptions(linewidth=console_width)
 
-# def split_data(year, pair, startingMonth, totalMonths):
+
+def shuffleLists(l1, l2):
+    rng = RandomState(1234)
+    rng.shuffle(l1)
+    rng.shuffle(l2)
+    return l1, l2
 
 
 base = 'priceData/'
 year = '2016'
 pair = 'BTC-USD'
 month = '2'
-totalMonth = 2
-
-
+totalMonth = 18
 
 csSize = 5
 
@@ -40,45 +48,35 @@ for i in range(totalMonth):
         month = str(int(month) - 12)
         year = str(int(year) + 1)
 
-
-tdata = np.array(tdata).T
-
-train = tdata[:15840]
-
-
-print(len(train))
-
-# test = tdata[578:]
-print(tdata.shape)
+train = np.array(tdata).T
+train = train[:-4]
 
 train_tme = pd.Series(train[:, 0])
-train_low = pd.Series(train[:, 1])
+train_open = pd.Series(train[:, 1])
 train_high = pd.Series(train[:, 2])
-train_open = pd.Series(train[:, 3])
+train_low = pd.Series(train[:, 3])
 train_close = pd.Series(train[:, 4])
 train_volume = pd.Series(train[:, 5])
 
-
+# Signals being used
 rsi = ta.momentum.rsi(train_close, n=14)
 tsi = ta.momentum.tsi(train_close, r=25, s=13)
 mfi = ta.momentum.money_flow_index(train_high, train_low, train_close, train_volume, n=14)
-
-easeOf = ta.volume.ease_of_movement(train_high, train_low, train_close, train_volume, n=20)
-
-avgDir = ta.trend.adx(train_high, train_low, train_close, n=14)
-macd = ta.trend.macd_diff(train_close, n_fast=12, n_slow=26, n_sign=9)
 macdSig = ta.trend.macd_signal(train_close, n_fast=12, n_slow=26, n_sign=9)
 
-
+# Signals for maybe later
+#
+# easeOf = ta.volume.ease_of_movement(train_high, train_low, train_close, train_volume, n=20)
+# avgDir = ta.trend.adx(train_high, train_low, train_close, n=14)
+# macd = ta.trend.macd_diff(train_close, n_fast=12, n_slow=26, n_sign=9)
 
 
 # vortex_pos = ta.trend.vortex_indicator_pos(train_high, train_low, train_close, n=14)
 # vortex_neg = ta.trend.vortex_indicator_neg(train_high, train_low, train_close, n=14)
 
 print(train_open.shape)
-print(train_close.shape)
+print(train.shape)
 
-"""
 ##############################################################
 ##                         SVM TIME
 ##############################################################
@@ -87,110 +85,128 @@ print(train_close.shape)
 ## Data Handling
 ##############################################################
 
+train_close = train_close[48:]
+rsi = rsi[48:]
+tsi = tsi[48:]
+mfi = mfi[48:]
+macdSig = macdSig[48:]
 
-# Build the data to import to SVM by chunking up the data into sets for classifying the next p amount of time based on
-# on the past h
+train_Total = functions.splitAndCompress(train_close, rsi, tsi, mfi, macdSig)
 
-batch_Size = 60
-num_Batches = int(len(train)/batch_Size)
+X = train_Total
 
+size = len(X)
+y = np.zeros(size)
 
-# This value keeps track of the index of batch so that the data lines up
-batch_index = 60
+print("The shape of X:", X.shape)
+print("The shape of y:", y.shape)
 
+# Generate y data for training
+for i in range(1, size):
 
-size = len(train_close)
-X = np.zeros((num_Batches, batch_Size, 9))
-y = np.zeros((num_Batches, 1))
+    currClose = X.item((i - 1, 0))
+    futureClose = X.item((i, 30))
+    diff = (futureClose - currClose) / currClose
 
-# Reshapes the datasets into 3D matracies for eazy conversion
-train_close_t = train_close.values.reshape([1, size, 1])
-train_open_t = train_open.values.reshape([1, size, 1])
-train_volume_t = train_volume.values.reshape([1, size, 1])
-
-
-rsi_t = rsi.values.reshape([1, size, 1])
-tsi_t = tsi.values.reshape([1, size, 1])
-mfi_t = mfi.values.reshape([1, size, 1])
-easeOf_t = easeOf.values.reshape([1, size, 1])
-avgDir_t = avgDir.values.reshape([1, size, 1])
-macd_t = macd.values.reshape([1, size, 1])
-macdSig_t = macdSig.values.reshape([1, size, 1])
-
-
-#  Range starts at 2 because the first frame is devoted to starting the calculations for
-#  all of the moving averages
-for i in range(1, num_Batches+1, 1):
-
-    # Populates X with with i sets of matrices size 1, ind , 1
-    X[i-1:i, ::, :1] = train_open_t[:1, batch_index - batch_Size:batch_index, :1]
-    X[i-1:i, ::, 1:2] = train_close_t[:1, batch_index - batch_Size:batch_index, :1]
-    X[i-1:i, ::, 2:3] = train_volume_t[:1, batch_index - batch_Size:batch_index, :1]
-    X[i-1:i, ::, 3:4] = rsi_t[:1, batch_index - batch_Size:batch_index, :1]
-    X[i-1:i, ::, 4:5] = tsi_t[:1, batch_index - batch_Size:batch_index, :1]
-    X[i-1:i, ::, 5:6] = easeOf_t[:1, batch_index - batch_Size:batch_index, :1]
-    X[i-1:i, ::, 6:7] = avgDir_t[:1, batch_index - batch_Size:batch_index, :1]
-    X[i-1:i, ::, 7:8] = macd_t[:1, batch_index - batch_Size:batch_index, :1]
-    X[i-1:i, ::, 8:9] = macdSig_t[:1, batch_index - batch_Size:batch_index, :1]
-
-    batch_index += 60
-
-
-print(X[:1, -5:, :])
-print((X.shape))
-
-
-for i in range(1, num_Batches + 1):
-
-    currClose = X.item((i-1, -1, 1))
-
-    if i < 10:
-        futureClose = X.item((i, 30, 1))
-
-    #print(currClose)
-    diff = (currClose - futureClose) / currClose
-    #print(diff)
-    if diff > 0.03:
-        y[i-1:i, :] = 1
-
+    if diff > 0.01:
+        y[i - 1] = 1
+    elif diff < -0.01:
+        y[i - 1] = -1
     else:
-        y[i-1:i, :] = 0
+        y[i - 1] = 0
 
-# print(y[:10,::,::])
-# print(X[:5, :10, :3])
+t_side_x = [];
+t_side_y = [];
+t_buy_x = [];
+t_buy_y = [];
+t_sell_x = [];
+t_sell_y = [];
+
+for i in range(len(X)):
+    if y[i] == 0:
+        t_side_x.append(X[i])
+        t_side_y.append(0)
+    elif y[i] == 1:
+        t_buy_x.append(X[i])
+        t_buy_y.append(1)
+    else:
+        t_sell_x.append(X[i])
+        t_sell_y.append(-1)
+
+t_side_x = np.array(t_side_x)
+t_side_y = np.array(t_side_y)
+t_buy_x =  np.array(t_buy_x)
+t_buy_y =  np.array(t_buy_y)
+t_sell_x = np.array(t_sell_x)
+t_sell_y = np.array(t_sell_y)
+
+t_side_x = t_side_x[:770]
+t_side_y = t_side_y[:770]
+t_buy_x =  t_buy_x[:770]
+t_buy_y =  t_buy_y[:770]
+t_sell_x = t_sell_x[:770]
+t_sell_y = t_sell_y[:770]
 
 
 
 
-# test = np.arange(50).reshape((1, 50, 1))
-# test2 = test[:1, 25:50, :1]
-# X[:1, :25, :1] = test[:1, :25, :1]
-# X[:1, 25:50, 1:2] = test2
-# print(X[:1, ::, :3])
+buys = len(t_buy_y)
+sells = len(t_sell_y)
+sideways = len(t_side_y)
 
+
+X = np.concatenate((t_side_x, t_buy_x, t_sell_x), axis=0)
+y = np.concatenate((t_side_y, t_buy_y, t_sell_y), axis=0)
+
+print(t_sell_x.shape)
+print(t_sell_y.shape)
+
+
+print(X.shape)
 print(y.shape)
 
-clf = svm.SVC(gamma=.0001, C=100)
-clf.fit(X, y)
+print("Buys:", buys, "Sells:", sells, "Sideways:", sideways)
 
-fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
-fig.subplots_adjust(bottom=0.2)
-ax1.plot(train_close[:], color='tab:green')
+from sklearn.linear_model import SGDClassifier
 
-ax2.plot(tsi[14:], color='tab:red')
-# ax2.plot(vortex_pos[14:], color='tab:red')
-# ax2.plot(vortex_neg[14:], color='tab:red')
-# ax2.plot(tsi_mid[startTime:], color='tab:blue')
-# ax2.plot(tsi_short[startTime:], color='tab:green')
-# ax2.plot(tsi_EMA[startTime:], color='tab:orange')
-# ax2.plot(tsi_EMA_Bollinger_High[startTime:], color='tab:brown')
-# ax2.plot(tsi_EMA_Bollinger_Low[startTime:], color='tab:green')
+X, y = shuffleLists(X, y)
 
-# ax2.axhline(33, color='darkgoldenrod')
-# ax2.axhline(0, color='darkgoldenrod')
-# ax2.axhline(-24, color='darkgoldenrod')
+train_X = X[:1700]
+train_y = y[:1700]
 
-# plt.show()
+test_X = X[1700:1701]
+test_y = y[1700:1701]
+
+clf1 = svm.SVC(gamma=.000001, C=10, kernel="rbf", cache_size=1000)
+clf1.fit(train_X, train_y)
+
+clf2 = SGDClassifier(loss='modified_huber', penalty='l2', max_iter=100, n_jobs=-1, learning_rate='optimal', n_iter=10)
+clf2.fit(train_X, train_y)
+
+# clf3 = svm.SVC(gamma=.0001, C=100, kernel="sigmoid")
+# clf3.fit(train_X, train_y)
+
+results1_y = clf1.predict(test_X)
+results2_y = clf2.predict(test_X)
+# results3_y = clf3.predict(test_X)
+
+print(results2_y)
+# print(test_y[:50])
+
+
 
 """
+from sklearn.metrics import accuracy_score
 
+acc1 = accuracy_score(test_y, results1_y)
+acc2 = accuracy_score(test_y, results2_y)
+# acc3 = accuracy_score(test_y, results3_y)
+
+
+print("Accuracy of model 1:", acc1)
+print("Accuracy of model 2:", acc2)
+# print("Accuracy of model 3:", acc3)
+
+joblib.dump(clf2, "SVM_Model.pkl")
+
+"""
